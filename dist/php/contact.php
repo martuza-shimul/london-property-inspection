98f4f18d1512e1c15a06719e3cb8506109ec02f3
Martuza <martuza.shimul@gmail.com>
2020-07-08 23:40:05 +0600

    quick enquiry form updated and configured with PHP

--- dist/php/contact.php
+++ dist/php/contact.php
@@ -4,7 +4,7 @@ if(empty($_POST['name'])  		   ||
    empty($_POST['email'])	      ||
    empty($_POST['phone']) 		   ||
    empty($_POST['postcode']) 		||
-   empty($_POST['message']) 		||
+   
    
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,43 +12,22 @@ if(empty($_POST['name'])  		   ||
 	return false;
    }
 
+ 
 
-   // File upload functionality
-$new_path = "No files Uploaded";
-$counter = 0;
-if(!empty($_FILES['uploaded_file']))
-  {
-    $path = "uploads/";
-    $path = $path . basename( $_FILES['uploaded_file']['name']);
-
-    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-    //   $new_path = "<a href=" .$path. ">Check here</a>";
-    //   $new_path = "<a href=" echo $path ">Check here</a>";
-      $new_path = "https://handymanco.co.uk/dist/php/".$path;
-    //   $counter = 1;
-  
-    } else{
-        echo "There was an error uploading the file, please try again!";
-    }
-  }
-
-// if(counter === 1){
-    
-// }   
-
-
+$service_name = $_POST['service'];
+$service_sub = $_POST['service-sub'];
 $name= $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $postcode = $_POST['postcode'];
-$message = $_POST['message'];
+
 
 	
 // Create the email and send the message
 
 $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your Handyman London website contact form .\n\n"."Here are the details:\n\nFirst Name: $name\n\n Postcode: $postcode \n\n Phone: $phone\n\nEmail: $email\n  \n Message: $message \n Uploaded File:" .$new_path ;
+$email_body = "You have received a new message from your London Property inspection website contact form .\n\n"."Here are the details:\n\ Name: $name\n\n Postcode: $postcode \n\n Phone: $phone\n\nEmail: $email\n  \n Service Name: $service_name \n Items: $service_sub" ;
 
 // if(!empty($new_path)){
 //   "<a href=" .new_$path. ">Check here</a>"
